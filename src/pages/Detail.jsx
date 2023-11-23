3a81623512b98edff51e87a21df5cd03ffabb499
yeahzzl <yeahzzl1018@gmail.com>
2023-11-23 14:09:33 +0900

    standard

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import TodoCard from "../components/TodoCard";
 
 function Detail() {
   const navigate = useNavigate();
 
   return (
     <div>
-      <Detail navigate={navigate} />
+      <TodoCard navigate={navigate} />
     </div>
   );
 }
